0b97e1d5daeec4ac169ca1d6f83b588d75edb22b
l0raxeo <fuzedcable@gmail.com>
2023-05-19 21:38:41 -0400

    Buffer between game states for Draft State and Build State

--- src/main/java/l0raxeo/sdw/scenes/game/initializers/DraftStateInitializer.java
+++ src/main/java/l0raxeo/sdw/scenes/game/initializers/DraftStateInitializer.java
@@ -1,10 +1,15 @@
 package l0raxeo.sdw.scenes.game.initializers;
 
+import l0raxeo.network.GameClient;
+import l0raxeo.network.MultiplayerHandler;
 import l0raxeo.rendering.Window;
+import l0raxeo.sdw.dataStructure.AssetPool;
+import l0raxeo.sdw.input.mouse.MouseManager;
 import l0raxeo.sdw.scenes.game.Game;
 import l0raxeo.sdw.scenes.game.GameState;
 import l0raxeo.sdw.scenes.game.map.items.ItemType;
 import l0raxeo.sdw.ui.GuiLayer;
+import l0raxeo.sdw.ui.GuiText;
 import l0raxeo.sdw.ui.components.GuiImageButton;
 import org.joml.Vector2i;
 
@@ -18,6 +23,7 @@ public class DraftStateInitializer extends GameStateInitializer
 
     private boolean drafting = false;
     private int NUM_DRAFTS = 2; // TODO this can be changed via game settings settings
+    private boolean readyToBuild = false;
 
     public DraftStateInitializer(Game gameScene)
     {
@@ -33,9 +39,11 @@ public class DraftStateInitializer extends GameStateInitializer
     @Override
     public void update(double dt)
     {
+
+
         if (NUM_DRAFTS <= 0) //after initial number of drafts are used it changes state
         {
-            GameState.setState(GameState.BUILD);
+            readyToBuild = true;
             return;
         }
 
@@ -44,6 +52,26 @@ public class DraftStateInitializer extends GameStateInitializer
             createDraftLineup();
             drafting = true;
         }
+    }
+
+    @Override
+    public void render(Graphics g)
+    {
+        if (readyToBuild)
+        {
+            GuiText.drawString(g, "Ready to build!", new Vector2i(Window.WINDOW_WIDTH / 2, Window.WINDOW_HEIGHT / 2), true, Color.GREEN, AssetPool.getFont("assets/fonts/default_font.ttf", 32));
+
+            if (MultiplayerHandler.isHosting())
+                promptBuildState(g);
+        }
+    }
+
+    private void promptBuildState(Graphics g)
+    {
+        GuiText.drawString(g, "Click any button to start", new Vector2i(Window.WINDOW_WIDTH / 2, (Window.WINDOW_HEIGHT / 2) + 32), true, Color.WHITE, AssetPool.getFont("assets/fonts/default_font.ttf", 16));
+
+        if (MouseManager.hasPressedInput())
+            GameClient.getInstance().sendData("gbs");
     }
 
     private void createDraftLineup()
@@ -70,12 +98,6 @@ public class DraftStateInitializer extends GameStateInitializer
                     }
             ));
         }
-    }
-
-    @Override
-    public void render(Graphics g)
-    {
-
     }
 
 }
