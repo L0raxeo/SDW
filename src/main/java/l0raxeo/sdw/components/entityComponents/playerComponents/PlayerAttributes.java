7c12c413d1dfcbcac0a8c2b8aae37b4745bedc7a
L0raxeo <67399230+L0raxeo@users.noreply.github.com>
2023-05-24 23:18:12 -0400

    synchronizing component ID test

--- src/main/java/l0raxeo/sdw/components/entityComponents/playerComponents/PlayerAttributes.java
+++ src/main/java/l0raxeo/sdw/components/entityComponents/playerComponents/PlayerAttributes.java
@@ -1,6 +1,5 @@
 package l0raxeo.sdw.components.entityComponents.playerComponents;
 
-import l0raxeo.network.GameClient;
 import l0raxeo.sdw.components.Component;
 
 public class PlayerAttributes extends Component
